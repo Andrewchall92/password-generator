5ca4f3e7fa344022061d95b3c95346f1fa14c892
moxnix <andrewchall92@gmail.com>
2023-09-26 23:47:11 -0700

    I added new variables with confirm prompts for the user to select which characters they want in their password.

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,17 +15,29 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword(){
+  // All variables possible for password generation
   var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
   var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numberChar = "0123456789";
   var specialChar = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-
+  // Defined as empty string to store data based on users choice
   var allChars ="";
   var password ="";
-
+  // Prompts user to enter number between 8-128 for password length
   var length = prompt("Enter a password length between 8 and 128");
   if (length <8 || length>128){
-    alert("Invalid length.Please enter a number between 8 and 128")
+    alert("Invalid length. Please enter a number between 8 and 128")
     return ""
   }
+  // Asks user to confrim which characters they would like to include in their password
+  var incLudeLowercase = confirm("Include lowercase characters?");
+  var incLudeUppercase = confirm("Include uppercase characters?");
+  var incLudeNumeric = confirm("Include numeric characters?");
+  var incLudeSpecial = confirm("Include special characters?");
+
+
+
+
+
+
 }
